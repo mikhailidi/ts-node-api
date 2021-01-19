127f3658daedee2bb3f4333aa6c2d1078c9e3c8f
mikhailidi <mikhailidi.p@gmail.com>
2021-01-19 21:05:37 +0100

    🌱 add fake tenantPayments data

---
+++ src/data/tenantPayment.ts
@@ -0,0 +1,68 @@
+export const tenantPayments = [
+  {
+    id: 1365,
+    contractId: 17689,
+    description: 'Rent payment',
+    value: 100,
+    time: '2016-12-09T00:00:00.00Z',
+    isImported: false,
+    createdAt: '2016-12-01T12:57:31.393Z',
+    updatedAt: '2016-12-09T12:57:31.393Z',
+    isDeleted: false
+  },
+  {
+    id: 1366,
+    contractId: 17689,
+    description: 'Rent to be paid 1',
+    value: -100,
+    time: '2016-12-09T00:00:00.00Z',
+    isImported: false,
+    createdAt: '2016-09-01T12:57:09.708Z',
+    updatedAt: '2016-12-09T12:57:09.709Z',
+    isDeleted: false
+  },
+  {
+    id: 1367,
+    contractId: 111222,
+    description: 'Rent to be paid 2',
+    value: -100,
+    time: '2016-12-01T00:00:00.00Z',
+    isImported: false,
+    createdAt: '2016-12-01T12:57:09.708Z',
+    updatedAt: '2016-12-09T12:57:09.709Z',
+    isDeleted: false
+  },
+  {
+    id: 1368,
+    contractId: 111222,
+    description: 'Rent to be paid 3',
+    value: 100,
+    time: '2018-12-09T00:00:00.00Z',
+    isImported: false,
+    createdAt: '2018-12-01T12:57:09.708Z',
+    updatedAt: '2018-12-09T12:57:09.709Z',
+    isDeleted: false
+  },
+  {
+    id: 1369,
+    contractId: 111222,
+    description: 'Rent payment 4',
+    value: 100,
+    time: '2018-12-09T00:00:00.00Z',
+    isImported: false,
+    createdAt: '2018-12-09T12:57:09.708Z',
+    updatedAt: '2020-12-09T12:57:09.709Z',
+    isDeleted: false
+  },
+  {
+    id: 1370,
+    contractId: 111222,
+    description: 'Rent to be paid 4',
+    value: -100,
+    time: '2019-06-09T00:00:00.00Z',
+    isImported: false,
+    createdAt: '2019-06-09T12:57:09.708Z',
+    updatedAt: '2020-12-09T12:57:09.709Z',
+    isDeleted: false
+  }
+];
