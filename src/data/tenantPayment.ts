3bf4e6e2eab652a5714ead0985e289690d551973
mikhailidi <mikhailidi.p@gmail.com>
2021-01-19 21:27:23 +0100

    🌱 adjust tenantPayments to the model interface

--- src/data/tenantPayment.ts
+++ src/data/tenantPayment.ts
@@ -1,13 +1,13 @@
-export const tenantPayments = [
+export const fakeTenantPayments = [
   {
     id: 1365,
     contractId: 17689,
     description: 'Rent payment',
     value: 100,
-    time: '2016-12-09T00:00:00.00Z',
+    time: new Date('2016-12-09T00:00:00.00Z'),
     isImported: false,
-    createdAt: '2016-12-01T12:57:31.393Z',
-    updatedAt: '2016-12-09T12:57:31.393Z',
+    createdAt: new Date('2016-12-01T12:57:31.393Z'),
+    updatedAt: new Date('2016-12-09T12:57:31.393Z'),
     isDeleted: false
   },
   {
@@ -15,10 +15,10 @@ export const tenantPayments = [
     contractId: 17689,
     description: 'Rent to be paid 1',
     value: -100,
-    time: '2016-12-09T00:00:00.00Z',
+    time: new Date('2016-12-09T00:00:00.00Z'),
     isImported: false,
-    createdAt: '2016-09-01T12:57:09.708Z',
-    updatedAt: '2016-12-09T12:57:09.709Z',
+    createdAt: new Date('2016-09-01T12:57:09.708Z'),
+    updatedAt: new Date('2016-12-09T12:57:09.709Z'),
     isDeleted: false
   },
   {
@@ -26,10 +26,10 @@ export const tenantPayments = [
     contractId: 111222,
     description: 'Rent to be paid 2',
     value: -100,
-    time: '2016-12-01T00:00:00.00Z',
+    time: new Date('2016-12-01T00:00:00.00Z'),
     isImported: false,
-    createdAt: '2016-12-01T12:57:09.708Z',
-    updatedAt: '2016-12-09T12:57:09.709Z',
+    createdAt: new Date('2016-12-01T12:57:09.708Z'),
+    updatedAt: new Date('2016-12-09T12:57:09.709Z'),
     isDeleted: false
   },
   {
@@ -37,10 +37,10 @@ export const tenantPayments = [
     contractId: 111222,
     description: 'Rent to be paid 3',
     value: 100,
-    time: '2018-12-09T00:00:00.00Z',
+    time: new Date('2018-12-09T00:00:00.00Z'),
     isImported: false,
-    createdAt: '2018-12-01T12:57:09.708Z',
-    updatedAt: '2018-12-09T12:57:09.709Z',
+    createdAt: new Date('2018-12-01T12:57:09.708Z'),
+    updatedAt: new Date('2018-12-09T12:57:09.709Z'),
     isDeleted: false
   },
   {
@@ -48,10 +48,10 @@ export const tenantPayments = [
     contractId: 111222,
     description: 'Rent payment 4',
     value: 100,
-    time: '2018-12-09T00:00:00.00Z',
+    time: new Date('2018-12-09T00:00:00.00Z'),
     isImported: false,
-    createdAt: '2018-12-09T12:57:09.708Z',
-    updatedAt: '2020-12-09T12:57:09.709Z',
+    createdAt: new Date('2018-12-09T12:57:09.708Z'),
+    updatedAt: new Date('2020-12-09T12:57:09.709Z'),
     isDeleted: false
   },
   {
@@ -59,10 +59,10 @@ export const tenantPayments = [
     contractId: 111222,
     description: 'Rent to be paid 4',
     value: -100,
-    time: '2019-06-09T00:00:00.00Z',
+    time: new Date('2019-06-09T00:00:00.00Z'),
     isImported: false,
-    createdAt: '2019-06-09T12:57:09.708Z',
-    updatedAt: '2020-12-09T12:57:09.709Z',
+    createdAt: new Date('2019-06-09T12:57:09.708Z'),
+    updatedAt: new Date('2020-12-09T12:57:09.709Z'),
     isDeleted: false
   }
 ];
