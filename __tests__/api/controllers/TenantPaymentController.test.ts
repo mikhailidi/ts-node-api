23df8c3f759758a1eaad1e051ace4cc7a15cf4cf
mikhailidi <mikhailidi.p@gmail.com>
2021-01-20 11:49:55 +0100

    ✅ success with no search specified

--- __tests__/api/controllers/TenantPaymentController.test.ts
+++ __tests__/api/controllers/TenantPaymentController.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import App from '../../../src/App';
 
 const CONTRACT_HAS_NO_PAYMENT_HISTORY = 123;
+const CONTRACT_HAS_PAYMENT_HISTORY = 111222;
 
 describe('Retrieving tenant payment history', () => {
   /**
@@ -21,6 +22,63 @@ describe('Retrieving tenant payment history', () => {
     expect(result.body).toMatchObject({
       sum: 0,
       items: []
+    });
+  });
+
+  it('Tests endpoint returns success with no search parameters specified', async () => {
+    const result = await request(App)
+      .get(`/contracts/${CONTRACT_HAS_PAYMENT_HISTORY}/payments/search`)
+      .send();
+
+    expect(result.status).toBe(200);
+    expect(result.body).toMatchObject({
+      sum: 0,
+      items: [
+        {
+          id: 1367,
+          contractId: 111222,
+          description: 'Rent to be paid 2',
+          value: -100,
+          time: '2016-12-01T00:00:00.000Z',
+          isImported: false,
+          createdAt: '2016-12-01T12:57:09.708Z',
+          updatedAt: '2016-12-09T12:57:09.709Z',
+          isDeleted: false
+        },
+        {
+          id: 1368,
+          contractId: 111222,
+          description: 'Rent to be paid 3',
+          value: 100,
+          time: '2018-12-09T00:00:00.000Z',
+          isImported: false,
+          createdAt: '2018-12-01T12:57:09.708Z',
+          updatedAt: '2018-12-09T12:57:09.709Z',
+          isDeleted: false
+        },
+        {
+          id: 1369,
+          contractId: 111222,
+          description: 'Rent payment 4',
+          value: 100,
+          time: '2018-12-09T00:00:00.000Z',
+          isImported: false,
+          createdAt: '2018-12-09T12:57:09.708Z',
+          updatedAt: '2020-12-09T12:57:09.709Z',
+          isDeleted: false
+        },
+        {
+          id: 1370,
+          contractId: 111222,
+          description: 'Rent to be paid 4',
+          value: -100,
+          time: '2019-06-09T00:00:00.000Z',
+          isImported: false,
+          createdAt: '2019-06-09T12:57:09.708Z',
+          updatedAt: '2020-12-09T12:57:09.709Z',
+          isDeleted: false
+        }
+      ]
     });
   });
 });
