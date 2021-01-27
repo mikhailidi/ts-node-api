5379fd1fb164dd77aab93b36a55ea1b7e1d0894b
mikhailidi <mikhailidi.p@gmail.com>
2021-01-27 21:35:51 +0100

    ✅ setup unit infrastracture for service unit test

---
+++ __tests__/unit/services/TenantPaymentService.test.ts
@@ -0,0 +1,30 @@
+import { mock, MockProxy, mockReset } from 'jest-mock-extended';
+import { TenantPayment } from '../../../src/models/TenantPayment';
+import { ITenantPaymentRepository } from '../../../src/repositories/TenantPaymentRepository';
+import TenantPaymentService from '../../../src/services/TenantPaymentService';
+
+let mockTenantPaymentModel: MockProxy<TenantPayment>;
+let mockRepository: MockProxy<ITenantPaymentRepository>;
+let testObject: TenantPaymentService;
+
+beforeEach(() => {
+  mockTenantPaymentModel = mock<TenantPayment>();
+  mockRepository = mock<ITenantPaymentRepository>();
+  testObject = new TenantPaymentService(mockRepository);
+});
+
+afterEach(() => {
+  mockReset(mockTenantPaymentModel);
+  mockReset(mockRepository);
+});
+
+describe('Test TenantPaymentService', () => {
+  it('Tests deletePayment method', () => {
+    const testTenantId = 123;
+
+    mockRepository.getPaymentById.calledWith(testTenantId).mockReturnValue(mockTenantPaymentModel);
+    mockRepository.deletePaymentById.calledWith(testTenantId);
+
+    testObject.deletePayment(testTenantId);
+  });
+});
