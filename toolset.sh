1f0dfd99abbb4c12452679228aa271c9bf097067
mikhailidi <mikhailidi.p@gmail.com>
2021-01-27 14:51:43 +0100

    👷 add bash script to interact with Docker

---
+++ toolset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+if [ -z "$1" ]
+then
+  echo "Choose one of the options";
+  echo " - bash -- login to bash container"
+  echo " - test -- run the tests from docker container"
+  exit
+fi
+
+case "$1" in 
+  bash)
+    # start an interactive bash inside the container
+    # the -l at the end stands for login shell that reads profile files (read man)
+    docker exec -it node-express-ts bash -l
+    ;;
+  test)
+    # executes the tests from docker container 
+    docker exec -it node-express-ts bash -c "yarn test"
+    ;;
+esac
