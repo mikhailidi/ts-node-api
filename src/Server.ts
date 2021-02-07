cea9501b8104b4fe6beef632df71f052f8d227fd
mikhailidi <mikhailidi.p@gmail.com>
2021-02-07 13:21:55 +0100

    ✨ add global middlewares

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, RequestHandler } from 'express';
 import http from 'http';
 import Controller from './controllers/Controller';
 import { IDatabase } from './interfaces/Database';
@@ -21,6 +21,12 @@ export default class Server {
   public loadControllers(controllers: Array<Controller>): void {
     controllers.forEach(controller => {
         this.app.use(controller.path, controller.setRoutes());
+    });
+  };
+
+  public loadMiddleware(middlewares: Array<RequestHandler>): void {
+    middlewares.forEach(middleware => {
+        this.app.use(middleware);
     });
   };
 
