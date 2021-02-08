e4f582fc02724c71ccfe6ee506ac5dd3c96cf899
mikhailidi <mikhailidi.p@gmail.com>
2021-02-08 11:41:56 +0100

    ✨ introduce local middlewares

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,4 +1,5 @@
-import { Response, Request, NextFunction, Router } from 'express';
+import { Response, Request, NextFunction, Router, RequestHandler } from 'express';
+import { createValidator, ExpressJoiInstance, ValidatedRequest } from 'express-joi-validation';
 import { IService } from '../interfaces/Service';
 
 export enum Methods {
@@ -11,33 +12,51 @@ export enum Methods {
 interface IRoute {
     path: string;
     method: Methods;
-    handler: (req: Request, res: Response, next: NextFunction) => void | Promise<void>;
+    handler: (req: Request | ValidatedRequest<any>, res: Response, next: NextFunction) => void | Promise<void>;
+    handlerMiddleware?: RequestHandler[];
 };
 
 export default abstract class Controller {
     public router: Router = Router();
     public abstract path: string;
     protected abstract readonly routes: Array<IRoute> = [];
-    protected service: IService;
+    protected validator: ExpressJoiInstance;
 
-    constructor(service: IService) {
-        this.service = service;
+    constructor(protected service: IService) {
+        this.validator = createValidator({
+            passError: true
+        });
     }
 
     public setRoutes = (): Router => {
         for (const route of this.routes) {
             switch (route.method) {
                 case 'GET':
-                    this.router.get(route.path, route.handler);
+                    this.router.get(
+                        route.path,
+                        route.handlerMiddleware ?? [],
+                        route.handler);
                     break;
                 case 'POST':
-                    this.router.post(route.path, route.handler);
+                    this.router.post(
+                        route.path,
+                        route.handlerMiddleware ?? [],
+                        route.handler
+                    );
                     break;
                 case 'PUT':
-                    this.router.put(route.path, route.handler);
+                    this.router.put(
+                        route.path,
+                        route.handlerMiddleware ?? [],
+                        route.handler
+                    );
                     break;
                 case 'DELETE':
-                    this.router.delete(route.path, route.handler);
+                    this.router.delete(
+                        route.path,
+                        route.handlerMiddleware ?? [],
+                        route.handler
+                    );
                     break;
                 default:
                     console.log('Not a valid method')
@@ -48,12 +67,22 @@ export default abstract class Controller {
         return this.router;
     }
 
+    /**
+     * 
+     * @param res 
+     * @param data 
+     */
     protected sendSuccess(res: Response, data: object): Response {
         return res.status(200).json(
             data
         );
     };
 
+    /**
+     * 
+     * @param res 
+     * @param message 
+     */
     protected sendNotFound(res: Response, message?: string): Response {
         return res.status(404).json({
             code: 404, // Should be Enum
@@ -61,6 +90,11 @@ export default abstract class Controller {
         });
     };
 
+    /**
+     * 
+     * @param res 
+     * @param message 
+     */
     protected sendError(res: Response, message?: string): Response {
         return res.status(500).json({
             message: message || 'Internal server error',
